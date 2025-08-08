8dd461a450156e9d5b1eb603688323b59842b1f3
ryuu-ssp <ryuu.sremsopon@gmail.com>
2025-08-08 12:20:35 +0700

    initial commit

--- t4.py
+++ t4.py
@@ -9,12 +9,18 @@ st.set_page_config(
     page_title="CASHFLOW MANAGEMENT",
     layout="wide"
 )
+col1, col2 = st.columns([9, 1])
+with col2:
+    st.image("logo.png", width=150)
+
+
+
 
 col1, col2, col3 = st.columns([1, 5, 1])
 
 with col2:
     st.markdown(
-        "<h1 style='text-align: center; margin: 0;'>CASHFLOW MANAGEMENT by quantifin</h1>",
+        "<h1 style='text-align: center; margin: 0;'>CASHFLOW MANAGEMENT</h1>",
         unsafe_allow_html=True
     )
 
